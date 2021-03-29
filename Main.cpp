0132c11a45b69bb4156964dee7ec6d982a21a8ab
polyester_ctrl <shunfreed2016@gmail.com>
2021-03-29 22:13:26 +0900

    ボタンを押して認識(仮)

--- Main.cpp
+++ Main.cpp
@@ -13,174 +13,195 @@ SIV3D_SET(EngineOption::Renderer::Direct3D11)
 
 struct Result
 {
-	s3d::String st;
-	s3d::Rect re;
-	double probability = 0.0;
+  s3d::String st;
+  s3d::Rect re;
+  double probability = 0.0;
 };
 
- Array<Result> RecognizeCharacterFromImage(Image handwritten)
+s3d::String RecognizeCharacterFromImage(Image handwritten,
+  std::string& text,
+  std::vector<cv::Rect>& boxes,
+  std::vector<std::string>& words,
+  std::vector<float>& confidences)
 {
-	Array<Result> results;
-	Result result;
-	// 画像読み込み
-	// const s3d::Image handwritten(U"example/hand.png");
-	cv::Mat image = s3d::OpenCV_Bridge::ToMatVec3bBGR(handwritten);
-	// cv::Mat image = cv::imread("Screenshot/hand.png");
-	// Print << image.size().width;
+  // 認識したデータを保存する 
+  // std::string text;
+  // std::vector<cv::Rect> boxes;
+  // std::vector<std::string> words;
+  Array<Result> results;
+  Result result;
+  // 画像読み込み
+  // const s3d::Image handwritten(U"example/hand.png");
+  cv::Mat image = s3d::OpenCV_Bridge::ToMatVec3bBGR(handwritten);
+  // cv::Mat image = cv::imread("Screenshot/hand.png");
+  // Print << image.size().width;
 
-	// グレースケール化
-	cv::Mat gray;
-	cv::cvtColor(image, gray, cv::COLOR_RGB2GRAY);
-	// 文字認識クラスのインスタンス生成
-	static cv::Ptr<cv::text::OCRTesseract> ocr = cv::text::OCRTesseract::create("tessdata", "jpn");
-	
-	// ホワイトリストを消す
-	ocr->setWhiteList("");
+  // グレースケール化
 
-	std::string text;
-	std::vector<cv::Rect> boxes;
-	std::vector<std::string> words;
-	std::vector<float> confidences;
-	// 文字認識の実行
-	ocr->run(gray, text, &boxes, &words, &confidences);
+  cv::Mat gray;
+  cv::cvtColor(image, gray, cv::COLOR_RGB2GRAY);
+  // 文字認識クラスのインスタンス生成
+  {
 
-	// 結果出力
-	// 文字のかたまりごとに出力
-	printf(" 文字      | 位置       | 大きさ     | 信頼度\n");
-	printf("-----------+------------+------------+----------\n");
-	for (int i = 0; i < boxes.size(); i++)
-	{
-		printf("%-10s | (%3d, %3d) | (%3d, %3d) | %f\n",
-			words[i].c_str(),
-			boxes[i].x, boxes[i].y,
-			boxes[i].width, boxes[i].height,
-			confidences[i]);
-		result.st = Unicode::FromUTF8(words[i]);
-		result.re.x = boxes[i].x, result.re.y = boxes[i].y;
-		result.re.w = boxes[i].width, result.re.h = boxes[i].height;
-		result.probability = confidences[i];
-		results.push_back(result);
-	}
-	String s = U"error";
-	if (boxes.size() > 0) 
-	{
-		// 文字コードを変換
-		s = Unicode::FromUTF8(text);
-	}
-	Print << s;
-	return results;
+    cv::Ptr<cv::text::OCRTesseract> ocr = cv::text::OCRTesseract::create("tessdata", "jpn");
+    // ホワイトリストを消す
+    ocr->setWhiteList("");
+
+    // 文字認識の実行
+    ocr->run(gray, text, &boxes, &words, &confidences);
+  }
+  /*
+  */
+  // 結果出力
+  // 文字のかたまりごとに出力
+  printf(" 文字      | 位置       | 大きさ     | 信頼度\n");
+  printf("-----------+------------+------------+----------\n");
+  for (int i = 0; i < boxes.size(); i++)
+  {
+    printf("%-10s | (%3d, %3d) | (%3d, %3d) | %f\n",
+      words[i].c_str(),
+      boxes[i].x, boxes[i].y,
+      boxes[i].width, boxes[i].height,
+      confidences[i]);
+    // result.st = Unicode::FromUTF8(words[i]);
+    // result.re.x = boxes[i].x, result.re.y = boxes[i].y;
+    // result.re.w = boxes[i].width, result.re.h = boxes[i].height;
+    // result.probability = confidences[i];
+    // results.push_back(result);
+  }
+  String s = U"error";
+  if (boxes.size() > 0)
+  {
+    // 文字コードを変換
+    s = Unicode::FromUTF8(text);
+  }
+  // Print << s;
+
+  return s;
 }
 
 
 
 void Main()
 {
-	// {
-		// Scene::SetBackground(ColorF{ 0.8, 0.9, 1.0 });
-		Scene::SetBackground(Palette::Black);
-		Scene::SetResizeMode(ResizeMode::Keep);
-		Window::SetStyle(WindowStyle::Sizable);
-		// Window::Resize(1280, 720);
 
-		// 画像読み込み
-		const s3d::Image handwritten(U"Screenshot/hand.png");
-		/**/
-		cv::Mat image = s3d::OpenCV_Bridge::ToMatVec3bBGR(handwritten);
-		// cv::Mat image = cv::imread("example\\hand.png");
-		// Print << image.size().width;
+  // Scene::SetBackground(ColorF{ 0.8, 0.9, 1.0 });
+  Scene::SetBackground(Palette::Black);
+  Scene::SetResizeMode(ResizeMode::Keep);
+  Window::SetStyle(WindowStyle::Sizable);
+  // Window::Resize(1280, 720);
+  std::string text;
+  std::vector<cv::Rect> boxes;
+  std::vector<std::string> words;
+  std::vector<float> confidences;
+  {
 
-		// グレースケール化
-		cv::Mat gray;
-		cv::cvtColor(image, gray, cv::COLOR_RGB2GRAY);
-		// 文字認識クラスのインスタンス生成
-		auto ocr = cv::text::OCRTesseract::create("tessdata", "jpn");
+    // 画像読み込み
+    const s3d::Image handwritten(U"Screenshot/hand.png");
+    /**/
+    cv::Mat image = s3d::OpenCV_Bridge::ToMatVec3bBGR(handwritten);
+    // cv::Mat image = cv::imread("example\\hand.png");
+    // Print << image.size().width;
 
-		// ホワイトリストを消す
-		ocr->setWhiteList("");
+    // グレースケール化
+    cv::Mat gray;
+    cv::cvtColor(image, gray, cv::COLOR_RGB2GRAY);
 
-		std::string text;
-		std::vector<cv::Rect> boxes;
-		std::vector<std::string> words;
-		std::vector<float> confidences;
-	{
-		// 文字認識の実行
-		ocr->run(gray, text, &boxes, &words, &confidences);
+    // 文字認識クラスのインスタンス生成
+    auto ocr = cv::text::OCRTesseract::create("tessdata", "jpn");
 
-		// 結果出力
+    // ホワイトリストを消す
+    ocr->setWhiteList("");
 
-		
-		// 文字のかたまりごとに出力
-		// printf(" 文字      | 位置       | 大きさ     | 信頼度\n");
-		// printf("-----------+------------+------------+----------\n");
-		for (int i = 0; i < boxes.size(); i++)
-		{
-			printf("%-10s | (%3d, %3d) | (%3d, %3d) | %f\n",
-				words[i].c_str(),
-				boxes[i].x, boxes[i].y,
-				boxes[i].width, boxes[i].height,
-				confidences[i]);
-		}
-		String s = U"hello";
-		if (boxes.size() > 0) {
-			// 文字コードを変換
-			s = Unicode::FromUTF8(text);
-		}
-		Print << s;
-		
-	}
-	/**/
-	AsyncTask<int32> task;
-	RecognizeCharacterFromImage(handwritten);
+    // 文字認識の実行
+    ocr->run(gray, text, &boxes, &words, &confidences);
+
+    // 結果出力
 
 
-	// スケッチから文字認識
+    // 文字のかたまりごとに出力
+    // printf(" 文字      | 位置       | 大きさ     | 信頼度\n");
+    // printf("-----------+------------+------------+----------\n");
+    for (int i = 0; i < boxes.size(); i++)
+    {
+      printf("%-10s | (%3d, %3d) | (%3d, %3d) | %f\n",
+        words[i].c_str(),
+        boxes[i].x, boxes[i].y,
+        boxes[i].width, boxes[i].height,
+        confidences[i]);
+    }
+    String s = U"hello";
+    if (boxes.size() > 0) {
+      // 文字コードを変換
+      s = Unicode::FromUTF8(text);
+    }
+    Print << s;
 
-	 // キャンバスのサイズ
-	constexpr Size canvasSize(600, 600);
 
-	// ペンの太さ
-	constexpr int32 thickness = 8;
+  }
 
-	// ペンの色
-	constexpr Color penColor = Palette::Orange;
+  AsyncTask<String> task;
+  // 画像読み込み
+  const s3d::Image handwritten(U"Screenshot/hand.png");
+  RecognizeCharacterFromImage(handwritten, text, boxes, words, confidences);
 
-	// 書き込み用の画像データを用意
-	Image dynamicImage(canvasSize, Palette::White);
 
-	// 表示用のテクスチャ（内容を更新するので DynamicTexture）
-	DynamicTexture texture(dynamicImage);
-	
-	
-	while (System::Update())
-	{
-		if (MouseL.pressed())
-		{
-			// 書き込む線の始点は直前のフレームのマウスカーソル座標
-			// （初回はタッチ操作時の座標のジャンプを防ぐため、現在のマウスカーソル座標にする）
-			const Point from = MouseL.down() ? Cursor::Pos() : Cursor::PreviousPos();
+  // スケッチから文字認識
 
-			// 書き込む線の終点は現在のマウスカーソル座標
-			const Point to = Cursor::Pos();
+   // キャンバスのサイズ
+  constexpr Size canvasSize(600, 600);
 
-			// image に線を書き込む
-			Line(from, to).overwrite(dynamicImage, thickness, penColor);
+  // ペンの太さ
+  constexpr int32 thickness = 8;
 
-			// 書き込み終わった image でテクスチャを更新
-			texture.fill(dynamicImage);
-		}
+  // ペンの色
+  constexpr Color penColor = Palette::Orange;
 
-		// 描いたものを消去するボタンが押されたら
-		if (SimpleGUI::Button(U"Clear", Vec2(640, 40), 120))
-		{
-			// 画像を白で塗りつぶす
-			dynamicImage.fill(Palette::White);
+  // 書き込み用の画像データを用意
+  Image dynamicImage(canvasSize, Palette::White);
 
-			// 塗りつぶし終わった image でテクスチャを更新
-			texture.fill(dynamicImage);
-		}
+  // 表示用のテクスチャ（内容を更新するので DynamicTexture）
+  DynamicTexture texture(dynamicImage);
 
-		// テクスチャを表示
-		texture.draw();
-	}
+
+  while (System::Update())
+  {
+    if (MouseL.pressed())
+    {
+      // 書き込む線の始点は直前のフレームのマウスカーソル座標
+      // （初回はタッチ操作時の座標のジャンプを防ぐため、現在のマウスカーソル座標にする）
+      const Point from = MouseL.down() ? Cursor::Pos() : Cursor::PreviousPos();
+
+      // 書き込む線の終点は現在のマウスカーソル座標
+      const Point to = Cursor::Pos();
+
+      // image に線を書き込む
+      Line(from, to).overwrite(dynamicImage, thickness, penColor);
+
+      // 書き込み終わった image でテクスチャを更新
+      texture.fill(dynamicImage);
+    }
+
+    // 描いたものを消去するボタンが押されたら
+    if (SimpleGUI::Button(U"Clear", Vec2(640, 40), 120))
+    {
+      // 画像を白で塗りつぶす
+      dynamicImage.fill(Palette::White);
+
+      // 塗りつぶし終わった image でテクスチャを更新
+      texture.fill(dynamicImage);
+    }
+
+    if (SimpleGUI::Button(U"Recognize", Vec2(640, 100), 120))
+    {
+      // task = RecognizeCharacterFromImage(dynamicImage, text, boxes, words, confidences);
+      task = CreateAsyncTask(RecognizeCharacterFromImage, std::ref(dynamicImage), std::ref(text), std::ref(boxes), std::ref(words), std::ref(confidences));
+    }
+    if (task.isReady())
+    {
+      Print << task.get();
+    }
+    // テクスチャを表示
+    texture.draw();
+  }
 }
-
